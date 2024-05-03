b8edcb65cffc51bc13a2916fe2e18f32fa6b447f
Rev1l00 <Oliverric@viken.no>
2024-05-03 08:31:01 +0200

    update to scraper.py

--- Spider/scraper.py
+++ Spider/scraper.py
@@ -1,9 +1,16 @@
 import requests as rq,mysql.connector
 from bs4 import BeautifulSoup as bs
 
-URL = "https://snl.no/ost"
+# Websiden som skal scrapes
+URL = "https://snl.no/ost/"
 
+def get_robots(url):
+    parts = url.split('/', 3)
+    if len(parts) >= 4:
+        return '/'.join(parts[:3]) + "/robots.txt"
+    
 
+# Funksjon som scraper siden og filtrerer ut den viktigste informasjonen
 def scrape(url:str) -> None:
     req = rq.get(url)
     soup = bs(req.text, 'html.parser')  
@@ -11,6 +18,6 @@ def scrape(url:str) -> None:
     print(soup.find("h1").text)
     print(soup.find(class_="article-text").text)
 
-scrape(URL)
-
-# Test
+# scrape(URL)
+result = get_robots(URL)
+print(result)
