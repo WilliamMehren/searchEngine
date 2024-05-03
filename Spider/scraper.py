f0d5505b52c1b46a94b92915ee4a87eed2fe0aab
Rev1l00 <Oliverric@viken.no>
2024-05-03 10:53:47 +0200

    scraper updates

--- Spider/scraper.py
+++ Spider/scraper.py
@@ -2,20 +2,33 @@ import requests as rq,mysql.connector
 from bs4 import BeautifulSoup as bs
 
 # Websiden som skal scrapes
-URL = "https://quotes.toscrape.com"
+# URL = "https://quotes.toscrape.com"
+URL = "https://www.geeksforgeeks.org/data-structures/"
 
 # Funksjon som henter ut alt inneholdet i robots.txt filen
-def get_robots(url):
-    parts = url.split('/', 3)
-    if len(parts) >= 4:
-        robots_url = '/'.join(parts[:3]) + "/robots.txt"
+def check_robots(url):
+    if url.count("/") > 2:
+        # Henter linken til nettsiden uten noen subsider
+        parts = url.split('/', 3)
+        if len(parts) >= 4:
+            robots_url = '/'.join(parts[:3]) + "/robots.txt"
+
+    # Scraper robots.txt og sender inneholdet til en txt fil
     req = rq.get(robots_url)
     robots_soup = bs(req.text, "html.parser")
-    return robots_soup
-    
+
+    text_file = open("Spider/current_robots.txt", "w")
+    text_file.write(robots_soup.text)
 
 # Funksjon som scraper siden og filtrerer ut den viktigste informasjonen
 def scrape(url:str) -> None:
+    # todo:
+    # Sjekke om url-en er i "robots.txt"
+    check_robots(url)
+    # Ikke legge til linker i køen som allered er der
+    # Ikke legge til linker i køen som har blitt scrapet nylig (Hent scrapetid fra database)
+    # Oppdater så den henter riktig informasjon som excerp
+
     req = rq.get(url)
     soup = bs(req.text, 'html.parser')
     print("URL: ", url)
@@ -25,15 +38,24 @@ def scrape(url:str) -> None:
     
     # Henter alle linker på siden og lagrer dem i en kø
     links = soup.find_all("a", href=True)
+    text_file = open("Spider/queue.txt", "a")
 
-    text_file = open("Spider/queue.txt", "w")
     for link in links:
         link = link["href"]
+        # Legger til https:// osv foran linker som ikke begynner med det
+        if link.startswith("/"):
+            base_url = url
+            if url.count("/") > 2:
+                parts = url.split('/', 3)
+                if len(parts) >= 4:
+                    base_url = '/'.join(parts[:3])
+            link = base_url + link
+            text_file.write(link + "\n")
+        
+        # Sørger for at bare riktige linker blir lagt inn i køen
         if link.startswith("https://"):
             text_file.write(link + "\n")
+
     text_file.close()
 
-
 scrape(URL)
-# robots = get_robots(URL)
-# print(robots)
