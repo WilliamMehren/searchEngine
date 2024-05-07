22f426bdbc920163940f1fb875278d45b385d99d
willi <willi@DERECTORYDESTRO>
2024-05-07 10:26:54 +0200

    better api

--- api/main.js
+++ api/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2")
-
+const util = require("util")
 const app = express();
 
 const conn = mysql.createConnection({
@@ -8,10 +8,14 @@ const conn = mysql.createConnection({
     user:"root",
     password:"Pickle123!"
 })
+const query = util.promisify(conn.query).bind(conn);
 
-
-app.get("/search",(req,res)=>{
-    let query = res.query.
+app.get("/search",async (req,res)=>{
+    let search = req.query.search;
+    let sites = await query(`SELECT * FROM browserdata.sites WHERE title LIKE '%${search}%' OR TEXT LIKE '%${search}%'`)
+    res.send(sites);
 });
 
-
+app.get("/lucky",async (req,res)=>{
+    let lucklist = []
+});
