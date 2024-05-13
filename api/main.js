b786fe37e33236549fbcb27a62f62c7fe011f09e
willi <willi@DERECTORYDESTRO>
2024-05-13 12:08:39 +0200

    search finished, start of images

--- api/main.js
+++ api/main.js
@@ -12,10 +12,13 @@ const query = util.promisify(conn.query).bind(conn);
 
 app.get("/search",async (req,res)=>{
     let search = req.query.search;
-    let sites = await query(`SELECT * FROM browserdata.sites WHERE title LIKE '%${search}%' OR TEXT LIKE '%${search}%'`)
+    let sites = await query(`SELECT * FROM browserdata.sites WHERE title LIKE '%${search}%' OR TEXT LIKE '%${search}%'`);
     res.send(sites);
 });
-
+app.get("/images",async ()=>{
+    let search = req.query.search
+    let images = await query(`SELECT * from browserdata.images WHERE `)
+});
 app.listen(5000,()=>{
-    console.log("listening on port 5000")
+    console.log("listening on port 5000");
 })
