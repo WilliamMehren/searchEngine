a180bde458a0bd05487aa3143453f7729bf4096a
willi <willi@DERECTORYDESTRO>
2024-05-24 10:34:38 +0200

    new theme, more api

--- Front-end/scripts/search.js
+++ Front-end/scripts/search.js
@@ -1,5 +1,5 @@
 
-
+const urlParams = new URLSearchParams(window.location.search);
 
 async function search(query){
     let res = await fetch(`http://10.1.120.50:5000/search?query=${query}`)
@@ -8,14 +8,19 @@ async function search(query){
 const searchBar = document.getElementById("searchBar");
 searchBar.addEventListener("submit",async (event) =>{
     event.preventDefault();
-    let results = await search(event.target[0].value);
+    showSearch(event.target)
+    
+});
+async function showSearch(form){
+    let results = await search(form[0].value);
     let resultBar = document.getElementById("searchResults");
-    console.log(resultBar.children)
     resultBar.innerHTML ="";
     for (let i = 0; i < results.length; i++){
         let result = results[i]
-        let container = document.createElement("a")
-        container.setAttribute("href",result.url)
+        let container = document.createElement("div")
+        container.setAttribute("class","searchResult")
+        let link = document.createElement("a")
+        link.setAttribute("href",result.url)
         let header = document.createElement("h2")
         if (result.name != "undefined"){
             header.innerText = result.name
@@ -26,11 +31,18 @@ searchBar.addEventListener("submit",async (event) =>{
         let paragraph = document.createElement("p")
         paragraph.innerText = result.text.slice(0,100)+"..."
         console.log(result)
-        container.appendChild(header)
-        container.appendChild(paragraph)
+        link.appendChild(header)
+        link.appendChild(paragraph)
+        container.appendChild(link)
         resultBar.appendChild(container)
         if (i == 19){
             break
         }
     }
-});
+}
+let query = urlParams.get("query")
+if (query != ""){
+    const searchBox = document.getElementById("search");
+    searchBox.value = query
+    showSearch(searchBar)
+}
