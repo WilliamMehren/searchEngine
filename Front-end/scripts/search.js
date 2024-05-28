ebd7da926aa04b08a9e12d186b0b2718da65a6f1
willi <willi@DERECTORYDESTRO>
2024-05-28 12:41:28 +0200

    merging

--- Front-end/scripts/search.js
+++ Front-end/scripts/search.js
@@ -1,8 +1,16 @@
 
 const urlParams = new URLSearchParams(window.location.search);
 let resIndex = 0
+let type = "image"
 async function search(query,index,limit){
-    let res = await fetch(`http://10.1.120.50:5000/search/site?query=${query}&index=${index}&limit=${limit}`)
+    let res
+    switch (type){
+
+        case "site":
+        res = await fetch(`http://10.1.120.50:5000/search/site?query=${query}&index=${index}&limit=${limit}`)
+        case "image":
+         res = await fetch(`http://10.1.120.50:5000/search/image?query=${query}&index=${index}&limit=${limit}`)
+    }
     
     return await res.json()
 }
@@ -16,46 +24,52 @@ async function showSearch(searchQuery){
     let results = await search(searchQuery,resIndex,20);
     let resultBar = document.getElementById("searchResults");
     resultBar.innerHTML ="";
-    for (let i = 0; i < results.results.length; i++){
-        let result = results.results[i]
-        let container = document.createElement("div")
-        container.setAttribute("class","searchResult")
-        let link = document.createElement("a")
-        link.setAttribute("href",result.url)
-        let header = document.createElement("h2")
-        if (result.name != "undefined"){
-            header.innerText = result.name
-        } else{
-            header.innerText = result.title
+    
+    switch(type){
+        case "site":
+        for (let i = 0; i < results.results.length; i++){
+                let result = results.results[i]
+                let container = document.createElement("div")
+                container.setAttribute("class","searchResult")
+                let link = document.createElement("a")
+                link.setAttribute("href",result.url)
+                let header = document.createElement("h2")
+                if (result.name != "undefined"){
+                    header.innerText = result.name
+                } else{
+                    header.innerText = result.title
+                }
+                
+                let paragraph = document.createElement("p")
+                paragraph.innerText = result.text.slice(0,100)+"..."
+                console.log(result)
+                link.appendChild(header)
+                link.appendChild(paragraph)
+                container.appendChild(link)
+                resultBar.appendChild(container)
+                if (i == 19){
+                    break
+                }
         }
-        
-        let paragraph = document.createElement("p")
-        paragraph.innerText = result.text.slice(0,100)+"..."
-        console.log(result)
-        link.appendChild(header)
-        link.appendChild(paragraph)
-        container.appendChild(link)
-        resultBar.appendChild(container)
-        if (i == 19){
-            break
-        }
-    }
-    let btnLimit = 5;
-    document.getElementById("pageSwitch").innerHTML = ""
-    if (results.amount[0].COUNT > 20){
-        for (let i = 0; i < Math.floor((results.amount[0].COUNT/20)); i++){
-            let btn = document.createElement("button")
-            btn.innerText = i
-            btn.onclick = async (event) => {
-                event.preventDefault()
-                resIndex = i*20
-                await showSearch(searchQuery)
-                console.log(resIndex)
+        let btnLimit = 500;
+        document.getElementById("pageSwitch").innerHTML = ""
+        if (results.amount[0].COUNT > 20){
+            for (let i = 0; i < Math.floor((results.amount[0].COUNT/20)); i++){
+                let btn = document.createElement("button")
+                btn.innerText = i
+                btn.onclick = async (event) => {
+                    event.preventDefault()
+                    resIndex = i*20
+                    await showSearch(searchQuery)
+                    console.log(resIndex)
+                }
+                document.getElementById("pageSwitch").appendChild(btn)
+                if (i > btnLimit){break}
             }
-            document.getElementById("pageSwitch").appendChild(btn)
-            if (i > btnLimit){break}
         }
+        case "image"
     }
+    
     
 }
 let query = urlParams.get("query")
